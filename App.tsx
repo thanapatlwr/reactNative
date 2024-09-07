83c4bb6884941419cfc3d092cc787b7438d64de3
thanapatlwr <thanapatlwr@gmail.com>
2024-09-07 11:24:03 +0700

    Menu7/9

--- App.tsx
+++ App.tsx
@@ -1,19 +1,30 @@
+import 'react-native-gesture-handler'
 import { View, Text } from 'react-native'
 import React from 'react'
+import {HeaderButtonsProvider} from 'react-navigation-header-buttons';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
 import HomeScreen from './screens/HomeScreen'
 import AboutScreen from './screens/AboutScreen'
+import MenuScreen from './screens/MenuScreen';
+import ProductScreen from './screens/ProductScreen';
+
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import CreatePostScreen from './screens/CreatePostScreen';
+import {createDrawerNavigator} from '@react-navigation/drawer'
 
-const App = () => {
-  const HomeStack = createNativeStackNavigator();
-  return (
-    <NavigationContainer>
+const HomeStack = createNativeStackNavigator();
+const ProductStack = createDrawerNavigator();
+const Drawer = createDrawerNavigator();
+
+function HomeStackScreen(){
+  return(
+    <SafeAreaProvider>
       <HomeStack.Navigator 
       initialRouteName='Home'
       screenOptions={{
-        headerStyle:{backgroundColor:'#6388ff'},
+        headerStyle:{backgroundColor:'#ffffff'},
         headerTintColor:'white',
         headerTitleStyle:{fontWeight:'bold'},
       }}
@@ -38,7 +49,39 @@ const App = () => {
         />
         <HomeStack.Screen name='CreatePost' component={CreatePostScreen}/>
       </HomeStack.Navigator>
-    </NavigationContainer>
+    </SafeAreaProvider>
+  )
+}
+function ProductStackScreen(){
+  return(
+      <ProductStack.Navigator 
+      initialRouteName='Home'
+      screenOptions={{
+        headerTitleStyle:{fontWeight:'bold'},
+      }}
+      >
+        <ProductStack.Screen
+          name='Product'
+          component={ProductScreen}
+        />
+      </ProductStack.Navigator>
+  )
+}
+const App = () => {
+  return (
+    <SafeAreaProvider>
+      <HeaderButtonsProvider stackType='native'>
+      <NavigationContainer>
+        <Drawer.Navigator
+          screenOptions={{headerShown:false}}
+          drawerContent={props => <MenuScreen{...props}/>}
+        >
+          <Drawer.Screen name='HomeStack' component={HomeStackScreen}/>
+          <Drawer.Screen name='ProductStack' component={ProductStackScreen}/>
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </HeaderButtonsProvider>
+    </SafeAreaProvider>
   
   )
 }
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,9 +1,49 @@
-import { StyleSheet,View, Text, Button } from "react-native";
-import React from "react";
+import { StyleSheet,View, Text, Button, Alert } from "react-native";
+import React, {useLayoutEffect} from "react";
 import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useNavigation } from "@react-navigation/native";
+import AppLogo from '../screens/AppLogo';
+import {
+  HeaderButton,
+  HeaderButtons,
+  Item,
+} from 'react-navigation-header-buttons';
+
+const MaterialHeaderButton = (props: any) =>(
+  // the `props` here come from <Item ... />
+  // you may access them and pass something else to `HeaderButton` if you like
+  <HeaderButton IconComponent={MaterialIcon} iconSize={23} {...props} />
+);
+
+const HomeScreen = (): React.JSX.Element => {
+  const navigation = useNavigation<any>();
+
+  useLayoutEffect(()=>{
+    navigation.setOptions({
+      title:'หน้าหลัก',
+      headerTitle:()=><AppLogo/>,
+      headerTitleAlign:'center',
+      headerLeft:()=>(
+        <HeaderButtons HeaderButtonComponent={MaterialHeaderButton}>
+          <Item title = "menu" iconName="menu"
+          onPress={()=>{
+            navigation.openDrawer();
+          }}
+          />
+        </HeaderButtons>
+      ),
+      headerRight:()=>(
+        <HeaderButtons HeaderButtonComponent={MaterialHeaderButton}>
+          <Item title = "logout" iconName="logout"
+          onPress={()=>{
+            Alert.alert("Log out","Close Menu")
+          }}
+          />
+        </HeaderButtons>)
+    })
+  },[navigation]);
 
 
-const HomeScreen = ({ navigation,route }: any): React.JSX.Element => {
   const gotoAbout = () => {
     navigation.navigate("About", {
       companyName: "Thai-Nichi Institute of Technology",
@@ -15,11 +55,6 @@ const HomeScreen = ({ navigation,route }: any): React.JSX.Element => {
       <MaterialIcon name="home" size={40} color='pink'/>
       <Text>HomeScreen</Text>
       <Button title="About us" onPress={gotoAbout} />
-      <View style={styles.postContainer}>
-        <Button title="Create post" onPress={()=> navigation.navigate("CreatePost")}/>
-        <Text style={styles.postText}>Post:</Text>
-        <Text style={styles.postContent}>{route.params?.post}</Text>
-      </View>
     </View>
   );
 };
